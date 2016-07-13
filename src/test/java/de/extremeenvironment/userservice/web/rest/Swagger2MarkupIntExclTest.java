3b63c42b6e8c586b187ac1c78c948858625083d8
David Steiman <d.steiman@stytex.de>
2016-07-14 00:38:43 +0200

    adds automated documentation generation

--- src/test/java/de/extremeenvironment/userservice/web/rest/Swagger2MarkupIntExclTest.java
+++ src/test/java/de/extremeenvironment/userservice/web/rest/Swagger2MarkupIntExclTest.java
@@ -2,12 +2,14 @@ package de.extremeenvironment.userservice.web.rest;
 
 import de.extremeenvironment.userservice.UserServiceApp;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -19,6 +21,7 @@ import springfox.documentation.staticdocs.SwaggerResultHandler;
 import javax.inject.Inject;
 import java.io.IOException;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +32,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("s2m")
 public class Swagger2MarkupIntExclTest {
 
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("build/asciidoc/snippets");
+
 
     @Inject
     private WebApplicationContext context;
@@ -38,9 +44,8 @@ public class Swagger2MarkupIntExclTest {
     @Before
     public void setup() throws IOException {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-            .build();
+            .apply(documentationConfiguration(this.restDocumentation)).build();
     }
-
 
     @Test
     public void convertSwaggerToAsciiDoc() throws Exception {
